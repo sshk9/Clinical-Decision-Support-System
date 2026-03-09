9884a5b5fcbd3541ceefb9fd4bfc8f1f6eb28198
Sara Shkoro <sarashkoro55@gmail.com>
2026-03-09 18:30:13 +0100

    Update main.py file.

--- src/main.py
+++ src/main.py
@@ -1,7 +1,15 @@
-print("MAIN: starting")
+import sys
+from PyQt5.QtWidgets import QApplication
+from src.ui.main_window import MainWindow
 
-from ui.main_window import run_app
+
+def main() -> None:
+    app = QApplication(sys.argv)
+    app.setStyle("Fusion")
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
+
 
 if __name__ == "__main__":
-    print("MAIN: calling run_app()")
-    run_app()
+    main()
